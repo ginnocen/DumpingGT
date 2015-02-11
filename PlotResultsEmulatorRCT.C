e7ac075c7e74330b846d218516572e1eb671d376
ginnocen <ginnocen@cern.ch>
2015-02-12 00:05:30 +0100

    add comparison

--- PlotResultsEmulatorRCT.C
+++ PlotResultsEmulatorRCT.C
@@ -17,68 +17,97 @@
 
 void PlotResultsEmulatorRCTRegions(int option==1){
 
-  TString inputfile;
-  if(option==1) inputfile="OutputEmulator/fileoutput.root";
-  if(option==2) inputfile="OutputUnpacker/fileoutput.root";
+  TString inputfileEmulator="OutputEmulator/fileoutput.root";
+  TString inputfileUnpacker="OutputUnpacker/fileoutput.root";
 
-  TFile*fout=new TFile(inputfile.Data(),"read");
+  TFile*foutEmulator=new TFile(inputfileEmulator.Data(),"read");
+  TFile*foutUnpacker=new TFile(inputfileUnpacker.Data(),"read");
 
-  TH1D *hlegacyregion_et = (TH1D*)fout->Get("hlegacyregion_et");
-  TH1D *hlegacyregion_gctEta= (TH1D*)fout->Get("hlegacyregion_gctEta");
-  TH1D *hlegacyregion_gctPhi= (TH1D*)fout->Get("hlegacyregion_gctPhi");
-  TH1D *hlegacyregion_crate= (TH1D*)fout->Get("hlegacyregion_crate");
-  TH1D *hlegacyregion_card= (TH1D*)fout->Get("hlegacyregion_card");
-  TH1D *hlegacyregion_index= (TH1D*)fout->Get("hlegacyregion_index");    
+  TH1D *hlegacyregion_etEmulator = (TH1D*)foutEmulator->Get("hlegacyregion_et");
+  TH1D *hlegacyregion_gctEtaEmulator= (TH1D*)foutEmulator->Get("hlegacyregion_gctEta");
+  TH1D *hlegacyregion_gctPhiEmulator= (TH1D*)foutEmulator->Get("hlegacyregion_gctPhi");
+  TH1D *hlegacyregion_crateEmulator= (TH1D*)foutEmulator->Get("hlegacyregion_crate");
+  TH1D *hlegacyregion_cardEmulator= (TH1D*)foutEmulator->Get("hlegacyregion_card");
+  TH1D *hlegacyregion_indexEmulator= (TH1D*)foutEmulator->Get("hlegacyregion_index");    
+
+  TH1D *hlegacyregion_etUnpacker = (TH1D*)foutUnpacker->Get("hlegacyregion_et");
+  TH1D *hlegacyregion_gctEtaUnpacker= (TH1D*)foutUnpacker->Get("hlegacyregion_gctEta");
+  TH1D *hlegacyregion_gctPhiUnpacker= (TH1D*)foutUnpacker->Get("hlegacyregion_gctPhi");
+  TH1D *hlegacyregion_crateUnpacker= (TH1D*)foutUnpacker->Get("hlegacyregion_crate");
+  TH1D *hlegacyregion_cardUnpacker= (TH1D*)foutUnpacker->Get("hlegacyregion_card");
+  TH1D *hlegacyregion_indexUnpacker= (TH1D*)foutUnpacker->Get("hlegacyregion_index");    
 
   TCanvas*canvas=new TCanvas("canvas","canvas",1500,600);  
   canvas->Divide(3,2);
 
   canvas->cd(1);
   //canvas_1->SetLogy();
-  hlegacyregion_et->GetXaxis()->SetRangeUser(0.,50.);
-  hlegacyregion_et->Draw();
+  hlegacyregion_etEmulator->GetXaxis()->SetRangeUser(0.,50.);
+  hlegacyregion_etEmulator->Draw();
+  hlegacyregion_etUnpacker->SetLineColor(2);
+  hlegacyregion_etUnpacker->Draw("same");
   canvas->cd(2);
   //canvas_2->SetLogy();
-  hlegacyregion_gctEta->GetXaxis()->SetRangeUser(0.,20.);
-  hlegacyregion_gctEta->Draw();
+  hlegacyregion_gctEtaEmulator->GetXaxis()->SetRangeUser(0.,20.);
+  hlegacyregion_gctEtaEmulator->Draw();
+  hlegacyregion_gctEtaUnpacker->SetLineColor(2);
+  hlegacyregion_gctEtaUnpacker->Draw("same");
+
   canvas->cd(3);
   //canvas_3->SetLogy();
-  hlegacyregion_gctPhi->GetXaxis()->SetRangeUser(0.,30.);
-  hlegacyregion_gctPhi->Draw();
+  hlegacyregion_gctPhiEmulator->GetXaxis()->SetRangeUser(0.,30.);
+  hlegacyregion_gctPhiEmulator->Draw();
+  hlegacyregion_gctPhiUnpacker->SetLineColor(2);
+  hlegacyregion_gctPhiUnpacker->Draw("same");
+
   canvas->cd(4);
   //canvas_4->SetLogy();
-  hlegacyregion_crate->GetXaxis()->SetRangeUser(0.,30.);
-  hlegacyregion_crate->Draw();
+  hlegacyregion_crateEmulator->GetXaxis()->SetRangeUser(0.,30.);
+  hlegacyregion_crateEmulator->Draw();
+  hlegacyregion_crateUnpacker->SetLineColor(2);
+  hlegacyregion_crateUnpacker->Draw("same");
+
   canvas->cd(5);
   //canvas_5->SetLogy();
-  hlegacyregion_card->GetXaxis()->SetRangeUser(0.,30.);
-  hlegacyregion_card->Draw();
+  hlegacyregion_cardEmulator->GetXaxis()->SetRangeUser(0.,30.);
+  hlegacyregion_cardEmulator->Draw();
+  hlegacyregion_cardUnpacker->SetLineColor(2);
+  hlegacyregion_cardUnpacker->Draw("same");
+
   canvas->cd(6);
   //canvas_6->SetLogy();
-  hlegacyregion_index->GetXaxis()->SetRangeUser(0.,30.);
-  hlegacyregion_index->Draw();
+  hlegacyregion_indexEmulator->GetXaxis()->SetRangeUser(0.,30.);
+  hlegacyregion_indexEmulator->Draw();
+  hlegacyregion_indexUnpacker->SetLineColor(2);
+  hlegacyregion_indexUnpacker->Draw("same");
 
-
-  if(option==1) canvas->SaveAs("PlotsEmulator/canvas_RCTRegions.pdf");
-  if(option==2) canvas->SaveAs("PlotsUnpacker/canvas_RCTRegions.pdf");
+  canvas->SaveAs("PlotsEmulatorVsUnpacker/canvas_regions.pdf");
 
 }
 
 void PlotResultsEmulatorRCTEmcands(int option=1){
 
-  TString inputfile;
-  if(option==1) inputfile="OutputEmulator/fileoutput.root";
-  if(option==2) inputfile="OutputUnpacker/fileoutput.root";
+  TString inputfileEmulator="OutputEmulator/fileoutput.root";
+  TString inputfileUnpacker="OutputUnpacker/fileoutput.root";
 
-  TFile*fout=new TFile(inputfile.Data(),"read");
+  TFile*foutEmulator=new TFile(inputfileEmulator.Data(),"read");
+  TFile*foutUnpacker=new TFile(inputfileUnpacker.Data(),"read");
 
-  TH1D *hlegacyemcand_rank = (TH1D*)fout->Get("hlegacyemcand_rank");
-  TH1D *hlegacyemcand_regionEta= (TH1D*)fout->Get("hlegacyemcand_regionEta");
-  TH1D *hlegacyemcand_regionPhi= (TH1D*)fout->Get("hlegacyemcand_regionPhi");
-  TH1D *hlegacyemcand_crate= (TH1D*)fout->Get("hlegacyemcand_crate");
-  TH1D *hlegacyemcand_card= (TH1D*)fout->Get("hlegacyemcand_card");
-  TH1D *hlegacyemcand_index= (TH1D*)fout->Get("hlegacyemcand_index");  
-  TH1D *hlegacyemcand_iso= (TH1D*)fout->Get("hlegacyemcand_iso");  
+  TH1D *hlegacyemcand_rankEmulator = (TH1D*)foutEmulator->Get("hlegacyemcand_rank");
+  TH1D *hlegacyemcand_regionEtaEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_regionEta");
+  TH1D *hlegacyemcand_regionPhiEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_regionPhi");
+  TH1D *hlegacyemcand_crateEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_crate");
+  TH1D *hlegacyemcand_cardEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_card");
+  TH1D *hlegacyemcand_indexEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_index");  
+  TH1D *hlegacyemcand_isoEmulator= (TH1D*)foutEmulator->Get("hlegacyemcand_iso");  
+
+  TH1D *hlegacyemcand_rankUnpacker = (TH1D*)foutUnpacker->Get("hlegacyemcand_rank");
+  TH1D *hlegacyemcand_regionEtaUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_regionEta");
+  TH1D *hlegacyemcand_regionPhiUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_regionPhi");
+  TH1D *hlegacyemcand_crateUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_crate");
+  TH1D *hlegacyemcand_cardUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_card");
+  TH1D *hlegacyemcand_indexUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_index");  
+  TH1D *hlegacyemcand_isoUnpacker= (TH1D*)foutUnpacker->Get("hlegacyemcand_iso");  
 
 
   TCanvas*canvas=new TCanvas("canvas","canvas",1800,600);  
@@ -86,35 +115,55 @@ void PlotResultsEmulatorRCTEmcands(int option=1){
 
   canvas->cd(1);
   //canvas_1->SetLogy();
-  hlegacyemcand_rank->GetXaxis()->SetRangeUser(0.,50.);
-  hlegacyemcand_rank->Draw();
+  hlegacyemcand_rankEmulator->GetXaxis()->SetRangeUser(0.,50.);
+  hlegacyemcand_rankEmulator->Draw();
+  hlegacyemcand_rankUnpacker->SetLineColor(2);
+  hlegacyemcand_rankUnpacker->Draw("same");
+
   canvas->cd(2);
   //canvas_2->SetLogy();
-  hlegacyemcand_regionEta->GetXaxis()->SetRangeUser(0.,20.);
-  hlegacyemcand_regionEta->Draw();
+  hlegacyemcand_regionEtaEmulator->GetXaxis()->SetRangeUser(0.,20.);
+  hlegacyemcand_regionEtaEmulator->Draw();
+  hlegacyemcand_regionEtaUnpacker->SetLineColor(2);
+  hlegacyemcand_regionEtaUnpacker->Draw("same");
+
   canvas->cd(3);
   //canvas_3->SetLogy();
-  hlegacyemcand_regionPhi->GetXaxis()->SetRangeUser(0.,30.);
-  hlegacyemcand_regionPhi->Draw();
+  hlegacyemcand_regionPhiEmulator->GetXaxis()->SetRangeUser(0.,30.);
+  hlegacyemcand_regionPhiEmulator->Draw();
+  hlegacyemcand_regionPhiUnpacker->SetLineColor(2);
+  hlegacyemcand_regionPhiUnpacker->Draw("same");
+
   canvas->cd(4);
   //canvas_4->SetLogy();
-  hlegacyemcand_crate->GetXaxis()->SetRangeUser(0.,4.);
-  hlegacyemcand_crate->Draw();
+  hlegacyemcand_crateEmulator->GetXaxis()->SetRangeUser(0.,4.);
+  hlegacyemcand_crateEmulator->Draw();
+  hlegacyemcand_crateUnpacker->SetLineColor(2);
+  hlegacyemcand_crateUnpacker->Draw("same");
+
   canvas->cd(5);
   //canvas_5->SetLogy();
-  hlegacyemcand_card->GetXaxis()->SetRangeUser(0.,4.);
-  hlegacyemcand_card->Draw();
+  hlegacyemcand_cardEmulator->GetXaxis()->SetRangeUser(0.,4.);
+  hlegacyemcand_cardEmulator->Draw();
+  hlegacyemcand_cardUnpacker->SetLineColor(2);
+  hlegacyemcand_cardUnpacker->Draw("same");
+
   canvas->cd(6);
   //canvas_5->SetLogy();
-  hlegacyemcand_index->GetXaxis()->SetRangeUser(0.,4.);
-  hlegacyemcand_index->Draw();
+  hlegacyemcand_indexEmulator->GetXaxis()->SetRangeUser(0.,4.);
+  hlegacyemcand_indexEmulator->Draw();
+  hlegacyemcand_indexUnpacker->SetLineColor(2);
+  hlegacyemcand_indexUnpacker->Draw("same");
+
   canvas->cd(7);
   //canvas_5->SetLogy();
-  hlegacyemcand_iso->GetXaxis()->SetRangeUser(0.,4.);
-  hlegacyemcand_iso->Draw();
+  hlegacyemcand_isoEmulator->GetXaxis()->SetRangeUser(0.,4.);
+  hlegacyemcand_isoEmulator->Draw();
+  hlegacyemcand_isoUnpacker->SetLineColor(2);
+  hlegacyemcand_isoUnpacker->Draw("same");
 
-  if(option==1) canvas->SaveAs("PlotsEmulator/canvas_emcands.pdf");
-  if(option==2) canvas->SaveAs("PlotsUnpacker/canvas_emcands.pdf");
+
+  canvas->SaveAs("PlotsEmulatorVsUnpacker/canvas_emcands.pdf");
 
 
 }
